a3c70a480bcdcfca1ea8043e8130b8b275948951
viktor <sharvik22@mail.ru>
2025-04-04 01:42:55 +0300

    Test3

--- providers.tf
+++ providers.tf
@@ -9,8 +9,8 @@ terraform {
 }
 
 provider "yandex" {
-  cloud_id  = var.cloud_id
-  folder_id = var.folder_id
-  zone      = var.default_zone
+  cloud_id                 = var.cloud_id
+  folder_id                = var.folder_id
+  zone                     = var.default_zone
   service_account_key_file = file("${path.module}/authorized_key.json")
 }
